dcd781f652ca89bedaac8ce4a66603ec5d9db91c
Victor Reis <victor.h.s.reis@gmail.com>
2020-10-19 19:38:42 -0300

    exercio 01

---
+++ exercicio01/main.go
@@ -0,0 +1,26 @@
+package main
+
+/*
+
+Utilizando o operador curto de declaração, atribua estes valores
+às variáveis com os identificadores "x", "y", e "z".
+    42
+    "James Bond"
+    true
+Agora demonstre os valores nestas variáveis, com:
+
+    Uma única declaração print
+    Múltiplas declarações print
+
+*/
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+	fmt.Println(x, " ", y, " ", z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
