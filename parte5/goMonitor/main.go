e44b3da98163c28bf50792ff0cc3ad8e1b9c3a1b
Victor Reis <victor.h.s.reis@gmail.com>
2021-04-16 23:55:22 -0300

    read a file

--- parte5/goMonitor/main.go
+++ parte5/goMonitor/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 const yellow = "\033[33m"
@@ -11,7 +14,7 @@ const red="\033[31m"
 const green  = "\033[32m"
 const purple = "\033[35m "
 const reset  = "\033[0m"
-const deleyInSeconds = 5
+const deleyInSeconds = 1
 
 func main(){
 	for {
@@ -58,12 +61,15 @@ func leComando() int {
 
 func iniciarMonitoramento(qdtTest int){
 	fmt.Println("Começando a monitorar")
-	sites := []string{"https://www.alura.com.br/",
-										"https://random-status-code.herokuapp.com/",
-										"https://herokuapp.com/",
-								}
+	// sites := []string{"https://www.alura.com.br/",
+	// 									"https://random-status-code.herokuapp.com/",
+	// 									"https://herokuapp.com/",
+	// 							}
+
+	sites := 	leArquivoSites()	
+
   for i:=1; i<=qdtTest; i++{
-		fmt.Println(yellow,"CICLO: ",i,reset)
+		fmt.Println(yellow,"\rCICLO: ",i,reset)
 		for  _,site := range sites{
 			testSite(site)
 		}
@@ -74,10 +80,34 @@ func iniciarMonitoramento(qdtTest int){
 }
 
 func testSite(site string){
-	resp, _:= http.Get(site)
-	if resp.StatusCode ==200{
-		fmt.Println(green,"+ Site:",reset,site," Carregado ok")
-	}else{
-		fmt.Println(red,"- Site:",reset,site,"com erro: ",resp.Status)
+	resp, err:= http.Get(site)
+	if err != nil{
+		fmt.Println(red,"Error:",err,reset)
 	}
+
+	if resp.StatusCode ==200{
+		fmt.Println(green,"\r+ Site:",reset,site," Carregado ok")
+	}else{
+		fmt.Println(red,"\r- Site:",reset,site,"com erro: ",resp.Status)
+	}
+}
+
+func leArquivoSites()[]string{
+
+	var sites []string
+	file,err := os.Open("sites.txt")
+	if err != nil{
+		fmt.Println(red,"Error:",err,reset)
+	}
+	leitor := bufio.NewReader(file)
+	for{
+		linha, err :=leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		sites = append(sites,linha)
+		if err == io.EOF{
+			break
+		}
+	}
+	file.Close()
+	return sites
 }
