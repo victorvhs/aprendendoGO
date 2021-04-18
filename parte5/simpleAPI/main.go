31f75d187e3b9b426d3af27a7b7a0cb2b4ddfef5
Victor Reis <victor.h.s.reis@gmail.com>
2021-04-18 19:16:11 -0300

    docker-compose com o banco de dados

--- parte5/simpleAPI/main.go
+++ parte5/simpleAPI/main.go
@@ -6,14 +6,33 @@ import (
 )
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
+type Produto struct{
+	Nome string
+	Descricao string
+	Preco float64
+	Qtd int
+}
 
 func main(){
 	http.HandleFunc("/",index)
 	http.ListenAndServe(":8000",nil)
-
 }
 
 func index(w http.ResponseWriter, r *http.Request){
-	templates.ExecuteTemplate(w,"Index",nil)
+	produtos := []Produto{
+		{Nome:"carro",
+		Descricao: "Um carro preto",
+		Preco:150000,
+		Qtd: 5},
+		
+		{"Apartemento",
+		"Pequeno",
+		980000,
+		1},
+		{"Fone","barulhento",15,5},
+
+	}
+	
+	templates.ExecuteTemplate(w,"Index",produtos)
 
 }
